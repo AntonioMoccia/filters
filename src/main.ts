7d08fe96847e5cf73a021ec1efa98f9d4ca3e36f
AntonioMoccia <moccia.ant@gmail.com>
2024-10-09 21:41:46 +0200

    resolved conflict

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import mock from "./MOCK_DATA.json";
 import { createFilter } from "./Filter";
-import mock from "./MOCK_DATA.json";
-import { createFilter } from "./Filter";
 
-const deepQuery: any = {
+
+
 const deepQuery: any = {
   logic: "or",
   term: [
@@ -20,13 +19,12 @@ const deepQuery: any = {
       term: [
         { field: "email", operator: "equal", value: "splumer2@imdb.com" },
         { field: "gender", operator: "equal", value: "Male" },
-        { field: "gender", operator: "equal", value: "Male" },
-      ],
-    },
+        { field: "gender", operator: "equal", value: "Male" }
+      ]
+
     },
   ],
 };
-
 const filters = [
   {
     name: "id",
@@ -52,31 +50,10 @@ const filter = createFilter({
   filters,
   data: mock.slice(0, 100),
   logic: "and",
-  watch: true
+  watch: true,
+  onFilterStateChange:(filter)=>{
+    console.log(filter);
+    
+  }
 });
-  {
-    name: "id",
-    field: "id",
-    operator: (row, state) => {
-      return row.id <= state.to && row.id >= state.from;
-    },
-    initialValue: {
-      from: undefined,
-      to: undefined,
-    },
-  },
-  {
-    name: "first_name",
-    field: "first_name",
-    operator: "equal",
-    initialValue: "Danette",
-  },
-];
-
-
-const filter = createFilter({
-  filters,
-  data: mock.slice(0, 100),
-  logic: "and",
-  watch: true
-});
+filter.handleFilterChange('first_name','dane')
